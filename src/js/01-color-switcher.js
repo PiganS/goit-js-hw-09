c920cf961e82e0a9cb83a03e87b6059733ad881e
PiganS <pigggggan@gmail.com>
2023-08-27 13:42:44 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  bodyRef: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop'),
+};
+refs.btnStart.addEventListener('click', onClickStart);
+let switcherId = null;
 
+function onClickStart() {
+  switcherId = setInterval(() => {
+    refs.bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStop.addEventListener('click', onClickStop);
+  refs.btnStart.disabled = true;
+}
+
+function onClickStop() {
+  clearInterval(switcherId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.removeEventListener('click', onClickStop);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
